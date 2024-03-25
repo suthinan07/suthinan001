982e404584f90e6fc2ec6a5a4b462cff466ed481
suthinan <suthinancheewapanich@gmail.com>
2024-03-25 16:18:37 +0700

    Update jj.py

--- jj.py
+++ jj.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 # Load data
 onlinefoods_data = pd.read_csv('./data/onlinefoods.csv')
 
@@ -9,7 +12,8 @@ st.write(onlinefoods_data.head(10))
 
 # Count by gender
 st.subheader('จำนวนเพศตามช่วงอายุ')
-st.pyplot(sns_countplot)
+plt.figure()
+sns_countplot = sns.countplot(x='Gender', data=onlinefoods_data)  
 plt.xlabel('เพศ')
 plt.ylabel('จำนวน')
 plt.xticks(rotation=45)
@@ -17,7 +21,8 @@ st.pyplot()
 
 # Sales by gender
 st.subheader('ยอดขายตามเพศ')
-st.pyplot(sns_barplot)
+plt.figure()
+sns_barplot = sns.barplot(x='Gender', y='Monthly Income', data=onlinefoods_data, estimator=sum)  
 plt.xlabel('เพศ')
 plt.ylabel('ยอดขายรวม')
 plt.xticks(rotation=45)
