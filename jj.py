d073faef15f8dbb76cd3a24b25ce31d8fd6bb966
suthinan <suthinancheewapanich@gmail.com>
2024-03-25 16:06:55 +0700

    a

--- jj.py
+++ jj.py
@@ -9,6 +9,7 @@ st.write(onlinefoods_data.head(10))
 
 # Count by gender
 st.subheader('จำนวนเพศตามช่วงอายุ')
+sns_barplot = sns.barplot(x='Gender', y='Sales', data=onlinefoods_data, estimator=sum)
 st.pyplot(sns_countplot)
 plt.xlabel('เพศ')
 plt.ylabel('จำนวน')
@@ -17,7 +18,6 @@ st.pyplot()
 
 # Sales by gender
 st.subheader('ยอดขายตามเพศ')
-sns_barplot = sns.barplot(x='Gender', y='Sales', data=onlinefoods_data, estimator=sum)
 st.pyplot(sns_barplot)
 plt.xlabel('เพศ')
 plt.ylabel('ยอดขายรวม')
